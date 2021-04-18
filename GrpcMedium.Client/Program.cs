c7c1c44571dcc80484cbcecc91998d3d751ce2e4
SuleymanLimann <suleymanliman28@gmail.com>
2021-04-18 22:07:17 +0300

    Add project files.

---
+++ GrpcMedium.Client/Program.cs
@@ -0,0 +1,95 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using GrpcMedium.Server;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using static GrpcMedium.Server.Customer;
+
+namespace GrpcMedium.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Güvenilir olmayan/geçersiz sertifikayla gRPC hizmetini çağırma
+            HttpClientHandler httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            //Güvenilir olmayan/geçersiz sertifikayla gRPC hizmetini çağırma
+
+            GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001",
+                new GrpcChannelOptions { HttpHandler = httpHandler });
+
+            CustomerClient client = new CustomerClient(channel);
+
+            #region Unary
+            CustomerRequest request = new CustomerRequest { CustomerId = 1 };
+            var callUnary = await client.GetCustomerUnaryAsync(request);
+            Console.WriteLine($"{callUnary.Name} ,{callUnary.Email},{callUnary.Adress},{callUnary.Age}");
+            #endregion
+
+            #region Server Streaming
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            using (var callServerStream = client.GetCustomerServerStream(new Empty()))
+            {
+                while (await callServerStream.ResponseStream.MoveNext(cancellationTokenSource.Token))
+                {
+                    //herhangi bir yerde iptal tokene gönderebiliriz
+                    //cancellationTokenSource.Cancel();
+
+                    CustomerResponse currentCustomer = callServerStream.ResponseStream.Current;
+                    Console.WriteLine($"{currentCustomer.Name} ,{currentCustomer.Email},{currentCustomer.Adress},{currentCustomer.Age}");
+                }
+            }
+            #endregion
+
+            #region Client Streaming
+
+            using (var callClientStream = client.GetCustomersClientStream())
+            {
+                for (int i = 1; i <= 3; i++)
+                {
+                    await callClientStream.RequestStream.WriteAsync(new CustomerRequest { CustomerId = i });
+                }
+                await callClientStream.RequestStream.CompleteAsync();// steam'i bitirdik
+
+                CustomersResponse response = await callClientStream;
+                foreach (var customer in response.Customer)
+                {
+                    Console.WriteLine($"{customer.Name} ,{customer.Email},{customer.Adress},{customer.Age}");
+                }
+            }
+            #endregion
+
+            #region Bi-Directional Streaming
+            CancellationTokenSource cancellationTokenSource2 = new CancellationTokenSource();
+            using (var callBiDirectionalStream = client.GetCustomersBiDirectionalStream())
+            {
+                Console.WriteLine("Başla");
+                var readTask = Task.Run(async () =>
+                {
+                     while (await callBiDirectionalStream.ResponseStream.MoveNext(cancellationTokenSource.Token))
+                    {
+                        Console.WriteLine(callBiDirectionalStream.ResponseStream.Current);
+                    }
+              
+                });
+              
+                for (int i = 1; i <= 3; i++)
+                {
+                    await callBiDirectionalStream.RequestStream.WriteAsync(new CustomerRequest { CustomerId = i });
+                    await Task.Delay(TimeSpan.FromSeconds(2));
+                }
+
+                await callBiDirectionalStream.RequestStream.CompleteAsync();
+                await readTask;
+                Console.WriteLine("Bitir");
+            }
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcMedium.Server/Services/CustomerService.cs
@@ -0,0 +1,79 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcMedium.Server.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+        private List<CustomerResponse> customerList;
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+            this.SetCustomerList();
+        }
+
+        public override async Task<CustomerResponse> GetCustomerUnary(CustomerRequest request, ServerCallContext context)
+        {
+            CustomerResponse customerResponce = customerList.FirstOrDefault(customer => customer.Id == request.CustomerId);
+
+            return await Task.FromResult(customerResponce);
+        }
+
+        public override async Task GetCustomerServerStream(Empty request, IServerStreamWriter<CustomerResponse> responseStream, ServerCallContext context)
+        {
+            foreach (var customer in customerList)
+            {
+                await responseStream.WriteAsync(customer);
+                await Task.Delay(TimeSpan.FromSeconds(5), context.CancellationToken);
+                //ServerCallContext içinde CancellationToken var.
+                //Clientin işlemi iptal edip etmediğini anlamamıza yarayan bir token.5 saniye arayla stream ediyoruz
+                //Dikkat return yok
+                
+            }
+        }
+
+        public override async Task<CustomersResponse> GetCustomersClientStream(IAsyncStreamReader<CustomerRequest> requestStream, ServerCallContext context)
+        {
+            CustomersResponse customersResponse = new CustomersResponse();
+
+            await foreach (var customerRequest in requestStream.ReadAllAsync())
+            {
+                customersResponse.Customer.Add(customerList.FirstOrDefault(customer => customer.Id == customerRequest.CustomerId));
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+
+            return customersResponse;
+        }
+
+        public override async Task GetCustomersBiDirectionalStream(IAsyncStreamReader<CustomerRequest> requestStream, IServerStreamWriter<CustomerResponse> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine("Başladı");
+            while (await requestStream.MoveNext() && !context.CancellationToken.IsCancellationRequested)
+            {
+                CustomerRequest request = requestStream.Current;
+                CustomerResponse customer= customerList.FirstOrDefault(customer => customer.Id == request.CustomerId);
+                await responseStream.WriteAsync(customer);
+                await Task.Delay(TimeSpan.FromSeconds(5));
+                Console.WriteLine("Devam ediyor");
+                //Dikkat return yok
+            }
+            Console.WriteLine("Bitti");
+        }
+
+        private void SetCustomerList()
+        {
+            customerList = new List<CustomerResponse>
+            {
+                new CustomerResponse { Id=1,Name="Süleyman Liman", Email="suleymanliman0@gmail.com",Adress="İstanbul",Age=28,IsActive=true },
+                new CustomerResponse { Id=2,Name="Yunus Dönmez", Email="yunusdonmez@gmail.com",Adress="İstanbul",Age=26,IsActive=false },
+                new CustomerResponse { Id=3,Name="Oğuzhan Mevsim", Email="oguzhanmevsim1@gmail.com",Adress="Kars",Age=25,IsActive=true },
+            };
+        }
+    }
+}
